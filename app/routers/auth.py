31d6a74ed2efcd6188d3187f3053d9876f9d36e7
Adrianov <adrianovriadi@gmail.com>
2024-05-29 12:34:46 +0700

    Success Create Doctor, Staff, Poly and Lab

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -122,11 +122,11 @@ async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db=Depends(get_db)
 ):
-    user = authenticate_user(form_data.username, form_data.password, db)
+    user = authenticate_user(form_data.user_email, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect user email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(hours=int(os.environ['ACCESS_TOKEN_EXPIRE_HOURS']))
